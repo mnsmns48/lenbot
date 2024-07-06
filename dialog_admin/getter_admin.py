ca334cb0693dc3869b75f489f5018f67e306b66a
mnsmns48 <getzboy@list.ru>
2024-07-06 22:30:17 +0300

    fix get_guests_getter

--- dialog_admin/getter_admin.py
+++ dialog_admin/getter_admin.py
@@ -75,7 +75,7 @@ async def post_info_getter(dialog_manager: DialogManager, session: AsyncSession,
 
     return {
         'date': data.date.strftime("%d.%m %H:%M"),
-        'text': data.text[:900],
+        'text': data.text[:850],
         'files': files,
         'info': data,
         'attachments_info': data.attachments_info,
